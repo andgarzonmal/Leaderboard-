04ab40725edd877c70e5520cb53651c6420c0562
andgarzonmal <andgarzonmal@unal.edu.co>
2022-06-15 16:36:18 -0500

    getting arr of objects from the api

--- src/Modules/getData.js
+++ src/Modules/getData.js
@@ -1,21 +1,14 @@
 const fetchUrl= "https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/UFEFOvdlSZM4VGo3r901/scores"
 
-// fetch(fetchUrl, { 
-//     method: "GET",
-//     headers: {
-//         "Content-Type": "application/json"
-//     }
-// }).then(function(response) {
-//     return response.text();
-// }).then(function(data) {
-//     console.log(data);
-// })
-
-
-const getData = async () => {
+export const getData = async () => {
     const response = await fetch(fetchUrl)
     const data = await response.json()
+    return data
 }
 
 
-export default getData
+export const deployData = async () => {
+    getData().then(result => 
+        console.log(result.result) )
+    
+}
