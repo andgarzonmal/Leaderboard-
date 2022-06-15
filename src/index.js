d27c31638b87c14b7c90e6e38a44f183336cf721
andgarzonmal <andgarzonmal@unal.edu.co>
2022-06-15 16:36:43 -0500

    re arrange import functions

--- src/index.js
+++ src/index.js
@@ -1,17 +1,6 @@
 import './style.css';
-import getData from './Modules/getData.js';
+import { getData, deployData } from './Modules/getData.js';
 
-// fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games', {
-//     method: 'POST',
-//     body: JSON.stringify({
-//         "name": "the new game name" 
-//     }),
-//     headers: {
-//         'Content-type': 'application/json; charset=UTF-8',
-//     },
-//     })
-//     .then((response) => response.json())
-//     .then((json) => console.log(json.result));
+console.log(deployData())
 
 
-getData()
