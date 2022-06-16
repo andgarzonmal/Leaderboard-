1fb6ce02d514473700040c5ec0a19e2940c4d8f3
andgarzonmal <andgarzonmal@unal.edu.co>
2022-06-15 21:40:59 -0500

    update module

--- src/Modules/createGame.js
+++ src/Modules/createGame.js
@@ -1,23 +1,20 @@
-// this module was created just to show how the id was obtained this module has no othe use in the app 
+// this module was created just to show how the id
+// was obtained this module has no othe use in the app
 
-let headersList = {
-  "Accept": "*/*",
-  "User-Agent": "Thunder Client (https://www.thunderclient.com)",
-  "Content-Type": "application/json"
- }
- 
- let bodyContent = JSON.stringify({ 
-   "name": "My cool new game" 
- });
- 
- fetch("https://us-central1-js-capstone-backend.cloudfunctions.net/api/games", { 
-   method: "POST",
-   body: bodyContent,
-   headers: headersList
- }).then(function(response) {
-   return response.text();
- }).then(function(data) {
-   console.log(data);
- })
+const headersList = {
+  Accept: '*/*',
+  'User-Agent': 'Thunder Client (https://www.thunderclient.com)',
+  'Content-Type': 'application/json',
+};
 
- // id received oeMwIfz4iX4Uqc9HOtgF
+const bodyContent = JSON.stringify({
+  name: 'My cool new game',
+});
+
+fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games', {
+  method: 'POST',
+  body: bodyContent,
+  headers: headersList,
+}).then((response) => response.text()).then((data) => data.result);
+
+// id received oeMwIfz4iX4Uqc9HOtgF
